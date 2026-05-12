56fd05858088b28fcaef7c7aaba9d2604e3b3e83
mah-fuz7 <mahafuz846@gmail.com>
2026-05-12 21:31:54 +0600

    set up react router and tailwind css

---
+++ src/Router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><h1>hellow world</h1></div>,
+  },
+]);
+export default router
